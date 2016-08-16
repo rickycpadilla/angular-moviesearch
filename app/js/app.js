ac22f5719089767195bd89ab0fedfb41ce51ca99
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-16 14:29:33 -0600

    refactored

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,5 @@
 var app = angular.module("movieApp", ['ngRoute']);
 
-var movies = {};
-
-app.controller("TopController", function($scope, $route, $http, $window){
-  $scope.Form = {};
-  // $scope.movies = {};
-  $scope.submitMov = function(input){
-    var movie = $scope.Form.movie;
-    $http.get('http://www.omdbapi.com/?s=' + movie).then(function(data){
-      // $scope.view.zenData = data;
-      // console.log(data.data.Search); // the array of results
-      movies.list = data.data.Search;
-      // console.log(movies.list);
-      $route.reload()
-      $window.location.href = '/#/results'
-  });
-};
-})
-
 app.config(function($routeProvider) {
     $routeProvider
       .when('/results', {
@@ -28,34 +10,4 @@ app.config(function($routeProvider) {
         templateUrl: 'partials/show.html',
         controller: 'ShowController'
       })
-      // .when('/resume', {
-      //   templateUrl: 'partials/home.html',
-      //   controller: 'ResController'
-      // })
 });
-
-app.controller('ResController', function($scope){
-  $scope.results = {};
-  $scope.results.list = movies.list
-  // console.log($scope.results.list);
-});
-
-app.controller('ShowController', function($scope, $http, $routeParams){
-  var movieID = $routeParams.id;
-  $scope.movieInfo = {};
-  $http.get('http://www.omdbapi.com/?i=' + movieID + '&plot=short&r=json').then(function(data){
-    console.log(data.data); // the array of results
-    $scope.movieInfo.info = data.data
-
-});
-});
-//
-// app.controller('ResController', function($scope){
-//   $scope.view = {};
-//   $scope.view.message = "resume!"
-// });
-
-// http://www.omdbapi.com/?s=ricky
-
-// http://www.omdbapi.com/?i=tt0371746&plot=short&r=json
-// longer request
---
+++ app/js/controllers.js
@@ -0,0 +1,27 @@
+var movies = {};
+
+app.controller("TopController", function($scope, $route, $http, $window){
+  $scope.Form = {};
+  $scope.submitMov = function(input){
+    var movie = $scope.Form.movie;
+    $http.get('http://www.omdbapi.com/?s=' + movie).then(function(data){
+      movies.list = data.data.Search;
+      $route.reload()
+      $window.location.href = '/#/results'
+    });
+  };
+})
+
+app.controller('ResController', function($scope){
+  $scope.results = {};
+  $scope.results.list = movies.list
+});
+
+app.controller('ShowController', function($scope, $http, $routeParams){
+  var movieID = $routeParams.id;
+  $scope.movieInfo = {};
+  $http.get('http://www.omdbapi.com/?i=' + movieID + '&plot=short&r=json').then(function(data){
+    $scope.movieInfo.info = data.data
+
+  });
+});
