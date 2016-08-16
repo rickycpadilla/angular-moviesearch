b05bf6eabe6fbe15465c8cd9cd7f48116e4fabe4
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-16 10:35:25 -0600

    API call working

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,31 @@
 var app = angular.module("movieApp", ['ngRoute']);
 
-app.config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/projects.html',
-        controller: 'ProjController'
-      })
-      .when('/bio', {
-        templateUrl: 'partials/bio.html',
-        controller: 'BioController'
-      })
-      .when('/resume', {
-        templateUrl: 'partials/home.html',
-        controller: 'ResController'
-      })
-});
+app.controller("TopController", function($scope, $http){
+  $scope.Form = {};
+  $scope.submitMov = function(input){
+    var movie = $scope.Form.movie;
+    $http.get('http://www.omdbapi.com/?s=' + movie).then(function(data){
+      // $scope.view.zenData = data;
+      console.log(data.data.Search);
+  });
+};
+})
+
+// app.config(function($routeProvider) {
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: 'partials/projects.html',
+//         controller: 'ProjController'
+//       })
+//       .when('/bio', {
+//         templateUrl: 'partials/bio.html',
+//         controller: 'BioController'
+//       })
+//       .when('/resume', {
+//         templateUrl: 'partials/home.html',
+//         controller: 'ResController'
+//       })
+// });
 
 app.controller('BioController', function($scope){
   $scope.view = {};
