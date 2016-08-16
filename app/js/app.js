737f6d8dd185ddf06e5b26917bcf455527759303
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-16 11:39:45 -0600

    Results route working

--- app/js/app.js
+++ app/js/app.js
@@ -1,45 +1,53 @@
 var app = angular.module("movieApp", ['ngRoute']);
 
-app.controller("TopController", function($scope, $http){
+var movies = {};
+
+app.controller("TopController", function($scope, $route, $http, $window){
   $scope.Form = {};
+  // $scope.movies = {};
   $scope.submitMov = function(input){
     var movie = $scope.Form.movie;
     $http.get('http://www.omdbapi.com/?s=' + movie).then(function(data){
       // $scope.view.zenData = data;
-      console.log(data.data.Search);
+      // console.log(data.data.Search); // the array of results
+      movies.list = data.data.Search;
+      console.log(movies.list);
+      $route.reload()
+      $window.location.href = '/#/results'
   });
 };
 })
 
-// app.config(function($routeProvider) {
-//     $routeProvider
-//       .when('/', {
-//         templateUrl: 'partials/projects.html',
-//         controller: 'ProjController'
-//       })
-//       .when('/bio', {
-//         templateUrl: 'partials/bio.html',
-//         controller: 'BioController'
-//       })
-//       .when('/resume', {
-//         templateUrl: 'partials/home.html',
-//         controller: 'ResController'
-//       })
-// });
-
-app.controller('BioController', function($scope){
-  $scope.view = {};
-  $scope.view.message = "bio!"
-});
-
-app.controller('ProjController', function($scope){
-  $scope.view = {};
-  $scope.view.message = "projects!"
+app.config(function($routeProvider) {
+    $routeProvider
+      .when('/results', {
+        templateUrl: 'partials/results.html',
+        controller: 'ResController'
+      })
+      .when('/movie', {
+        templateUrl: 'partials/show.html',
+        controller: 'ShowController'
+      })
+      // .when('/resume', {
+      //   templateUrl: 'partials/home.html',
+      //   controller: 'ResController'
+      // })
 });
 
 app.controller('ResController', function($scope){
-  $scope.view = {};
-  $scope.view.message = "resume!"
+  $scope.results = {};
+  $scope.results.list = movies.list
+  console.log($scope.results.list);
 });
+
+app.controller('ShowController', function($scope){
+  $scope.view = {};
+  $scope.view.message = "projects!"
+});
+//
+// app.controller('ResController', function($scope){
+//   $scope.view = {};
+//   $scope.view.message = "resume!"
+// });
 
 // http://www.omdbapi.com/?s=ricky
